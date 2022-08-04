7a88234bd062751941fb163af24a5c25b48c3edb
Jake Southall <jake321southall@aol.com>
2022-08-04 17:50:48 +0100

    completed inital eda and created first linear model

---
+++ script.py
@@ -0,0 +1,130 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+# load and investigate the data here:
+tennis_df = pd.read_csv('tennis_stats.csv')
+print(tennis_df.head())
+print(tennis_df.info())
+
+
+
+#EDA:
+#Finding the features with the highest correlations
+corr_df = tennis_df.corr().abs()
+#print(corr_df)
+#print(corr_df.unstack())
+#print(corr_df.unstack().sort_values(kind="quicksort")[:-len(tennis_df.columns)])
+
+
+#This one is pretty obvious
+sns.scatterplot(y=tennis_df.ServiceGamesPlayed , x=tennis_df.ReturnGamesPlayed, alpha=0.3)
+#plt.show()
+#plt.clf()
+
+sns.scatterplot(y=tennis_df.BreakPointsOpportunities, x=tennis_df.ReturnGamesPlayed, alpha=0.3)
+#plt.show()
+#plt.clf()
+
+sns.scatterplot(y=tennis_df.ReturnGamesPlayed, x=tennis_df.BreakPointsOpportunities, alpha=0.3)
+#plt.show()
+plt.clf()
+
+#Finding the features with the highest correlations to winnings
+corr_df_winnings = corr_df['Winnings']
+print(corr_df_winnings)
+#Wins and games played is pretty obvious, but interestingly there is 0.9 correlation between winnings and 
+#break point oppurtunities
+
+sns.scatterplot(y=tennis_df.Winnings, x=tennis_df.BreakPointsOpportunities, alpha=0.3)
+plt.show()
+plt.clf()
+
+#creating a linear regression model with single feature
+feature = tennis_df[['BreakPointsOpportunities']]
+target = tennis_df[['Winnings']]
+
+#Creating train and test sets
+feature_train, feature_test, target_train, target_test = train_test_split(feature, target, train_size=0.8)
+
+#instantiating the model and fitting to the training sets
+model = LinearRegression()
+model.fit(feature_train, target_train)
+
+#computing the score on the test sets
+score = model.score(feature_test, target_test)
+print(f'The score of this model is: {score}.')
+
+#lets visulise this
+predictions = model.predict(feature_test)
+plt.scatter(target_test,predictions, alpha=0.4)
+plt.show() 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## perform single feature linear regressions here:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## perform two feature linear regressions here:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## perform multiple feature linear regressions here:
