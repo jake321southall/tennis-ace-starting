175f8cfc0251ca0191bdb37a5776a2f632a94134
Jake Southall <jake321southall@aol.com>
2022-08-04 18:00:14 +0100

    completed aces linear model

--- script.py
+++ script.py
@@ -59,8 +59,37 @@ print(f'The score of this model is: {score}.')
 
 #lets visulise this
 predictions = model.predict(feature_test)
-plt.scatter(target_test,predictions, alpha=0.4)
+plt.scatter(target_test,predictions, alpha=0.3)
+plt.xlabel('actual winnings')
+plt.ylabel('predicted winnings')
+plt.title('prediction winnings vs actual winnings using break points opportunities')
 plt.show() 
+plt.clf()
+
+#Let's do this again for one more feature
+feature2 = tennis_df[['Aces']]
+target2 = tennis_df[['Winnings']]
+
+#Creating train and test sets
+feature_train2, feature_test2, target_train2, target_test2 = train_test_split(feature2, target2, train_size=0.8)
+
+#instantiating the model and fitting to the training sets
+model = LinearRegression()
+model.fit(feature_train2, target_train2)
+
+#computing the score on the test sets
+score = model.score(feature_test2, target_test2)
+print(f'The score of this model is: {score}.')
+
+#lets visulise this
+predictions2 = model.predict(feature_test2)
+plt.scatter(target_test2 ,predictions2 , alpha=0.3)
+plt.xlabel('actual winnings')
+plt.ylabel('predicted winnings')
+plt.title('prediction winnings vs actual winnings using Aces')
+plt.show() 
+plt.clf()
+
 
 
 
